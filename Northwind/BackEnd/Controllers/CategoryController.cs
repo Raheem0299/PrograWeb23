1be78e5e0d4bffe23f968716afc105fc1f6709a4
raheem0299 <raheem0299@gmail.com>
2023-06-19 18:34:09 -0600

    s

---
+++ Northwind/BackEnd/Controllers/CategoryController.cs
@@ -0,0 +1,77 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+
+        private ICategoryDAL categoryDAL;
+
+        #region Constructores
+
+        public CategoryController()
+        {
+            categoryDAL = new CategoryDALImpl();
+        }
+
+        #endregion
+
+
+        #region Consultas
+
+        // GET: api/<CategoryController>
+        [HttpGet]
+        public JsonResult Get()
+        {
+            IEnumerable<Category> categories = categoryDAL.GetAll();
+
+            return new JsonResult(categories);
+        }
+
+        // GET api/<CategoryController>/5
+        [HttpGet("{id}")]
+        public JsonResult Get(int id)
+        {
+            Category category = categoryDAL.Get(id);
+
+            return new JsonResult(category);
+        }
+        #endregion
+
+        #region Agregar
+
+
+        // POST api/<CategoryController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        #endregion
+
+        #region Modificar
+
+        // PUT api/<CategoryController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+        #endregion
+
+        #region Eliminar
+
+        // DELETE api/<CategoryController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        #endregion
+    }
+}
